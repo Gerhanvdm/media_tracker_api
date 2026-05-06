9dad5c26f841179eb6fed9b1caf4dbbbb4db2d31
Gerhan van der Merwe <gerhan@4gl.co.za>
2026-05-06 15:54:40 +0200

    removed wrong folder

--- api/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.base import Base
-from app.models.user import User
-from app.models.media import MediaItem, MediaType
-from app.models.rating import MediaRating
-
-__all__ = ["Base", "User", "MediaItem", "MediaType", "MediaRating"]
